c357df0f9f72f913b2c859cd86eecb28963cd75a
Mark Blackmore <mblackmo@yahoo.com>
2018-01-10 15:17:19 -0800

    Start section one.

---
+++ quick_refresher.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: Quick refresher
+#' author: "Mark Blackmore"
+#' date: "`r format(Sys.Date())`"
+#' output: 
+#'   github_document:
+#'     toc: true
+#'     
+#' ---
+#'
+#' ### Writing a function
+
